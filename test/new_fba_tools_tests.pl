cd68af283f96bfdf0dd23d43be8527debdab6c4c
JamesJeffryes <jamesgjeffryes@gmail.com>
2017-10-09 08:59:30 -0500

    TASK-1146: \r as a newline char is now accepted

--- test/new_fba_tools_tests.pl
+++ test/new_fba_tools_tests.pl
@@ -31,7 +31,7 @@ sub get_ws_name {
     return $ws_name;
 }
 #=head
-=cut
+
 # build_metabolic_model
 lives_ok{
         $impl->build_metabolic_model({
@@ -437,22 +437,22 @@ lives_ok{
 # tsv_file_to_phenotype_set
 lives_ok{
         $impl->tsv_file_to_phenotype_set({
-            phenotype_set_file => {path => "/kb/module/test/data/phenotype_simulation.tsv"},
-	        phenotype_set_name => "tsv_phenotypeset",
+            phenotype_set_file => {path => "/kb/module/test/data/JZ_UW_Phynotype_Set_test.txt"},
+	        phenotype_set_name => "return_delimited",
 	        workspace_name => get_ws_name(),
 	        genome => "8248/9/1"
         })
-    } 'import phenotype set from tsv';
+    } 'TSV to Phenotype Set: import return delimented';
 
 lives_ok{
     $impl->tsv_file_to_phenotype_set({
                 phenotype_set_file => {path => "/kb/module/test/data/NewPhenotypeSet.tsv"},
                 phenotype_set_name => "tsv_phenotypeset",
-                workspace_name => "jjeffryes:narrative_1502586048308",
+                workspace_name => get_ws_name(),
                 genome => "7601/4/1"
         })
-    }, 'TSV to Phenotype Set';
-=cut
+    }, 'TSV to Phenotype Set: custom columns';
+
 dies_ok{
         $impl->tsv_file_to_phenotype_set({
             phenotype_set_file => {path => "/kb/module/test/data/EmptyPhenotypeSet.tsv"},
@@ -461,7 +461,7 @@ dies_ok{
             genome => "7601/4/1"
         })
     } 'import empty phenotype set fails';
-die;
+
 # phenotype_set_to_tsv_file
 lives_ok{
         $impl->phenotype_set_to_tsv_file({
