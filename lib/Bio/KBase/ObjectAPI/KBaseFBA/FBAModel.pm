2f78251895da17588c6b51b407ef6c258637273a
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-02-28 14:53:36 -0600

    Gapfilling will not return error if gapfilling fails, rather just print a message

--- lib/Bio/KBase/ObjectAPI/KBaseFBA/FBAModel.pm
+++ lib/Bio/KBase/ObjectAPI/KBaseFBA/FBAModel.pm
@@ -1370,7 +1370,9 @@ sub add_gapfilling {
 	my $biomass_removals = $args->{object}->biomassRemovals();
 	my $brkeys = [keys(%{$biomass_removals})];
 	if (@{$brkeys} > 0) {
-		Bio::KBase::utilities::error("Gapfilling impossible without modifying biomass reaction!");
+		my $tbl = "<h3>Gapfilling was unable to find a solution using the chosen media.</h3>";
+		return Bio::KBase::utilities::gapfilling_html_table({message => $tbl,append => 0});
+		# Rest is of code block is skipped
 		my $biomass = "bio1";
 		if (!defined($biomass_removals->{bio1})) {
 			$biomass = $brkeys->[0];
