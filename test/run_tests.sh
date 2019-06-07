c230695a73bf74b63dee73e6f96dc32936bc7ecd
Christopher Henry <chenry@mcs.anl.gov>
2019-06-07 15:42:43 -0500

    Tweaking tests to make sure travis can run

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_dir=$(dirname "$(readlink -f "$0")")
-export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
+export KB_DEPLOYMENT_CONFIG=$script_dir/../testdeploy.cfg
 export KB_AUTH_TOKEN=`cat /kb/module/work/token`
 export PERL5LIB=$script_dir/../lib:$PATH:$PERL5LIB
 cd $script_dir/../test
