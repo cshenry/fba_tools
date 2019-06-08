de09ad12cbb4a39803eb3414f21e1be1198d6f9e
Christopher Henry <chenry@mcs.anl.gov>
2019-06-08 10:45:40 -0500

    Debugging travis build testing

--- test/new_fba_tools_tests.pl
+++ test/new_fba_tools_tests.pl
@@ -12,11 +12,12 @@ use fba_tools::fba_toolsImpl;
 local $| = 1;
 my $token = $ENV{'KB_AUTH_TOKEN'};
 my $config_file = $ENV{'KB_DEPLOYMENT_CONFIG'};
+print "Config file:".$ENV{'KB_DEPLOYMENT_CONFIG'}."\n"
 my $config = new Config::Simple($config_file)->get_block('fba_tools');
 my $ws_url = $config->{"workspace-url"};
+print "Workspace URL:".$ws_url."\n"
 my $ws_name = undef;
 my $test_ws = "fba_tools_unittests_ws";
-#my $ws_client = new Bio::KBase::workspace::Client($ws_url,token => $token);
 my $ws_client = Workspace::WorkspaceClient->new($ws_url,token => $token);
 my $auth_token = Bio::KBase::AuthToken->new(token => $token, ignore_authrc => 1);
 my $ctx = LocalCallContext->new($token, $auth_token->user_id);
